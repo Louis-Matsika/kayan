9cb0f47687a68b05e3b8520f7478f0406fc713e2
Louis Matsika <louis.matsika@bbc.co.uk>
2022-08-12 22:00:21 +0100

    set up home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-
+        <Route path="/" element={<Home />} />
       </Routes>     
     </Router>
   );
