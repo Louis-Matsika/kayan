7a116c0f5fbc5522eb4075f7215a470301f1fd45
Maiken Gunnerød <maiken.gunneroed@gmail.com>
2022-08-16 16:48:53 +0200

    add basic homepage layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return(
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <div>
+      <header class="header">
+        <a href="#">
+          <img
+            src="site-content/omologo.png"
+            alt="Logo"
+            class="header-logo"
+          ></img>
+        </a>
+      </header>
+      <main class="main-content">
+        <section class="section-logo">
+          <img
+            src="site-content/toni_phases.png"
+            alt="Logo"
+            class="main-logo"
+          ></img>
+        </section>
+        <section class="section-title">
+          <h1 class="main-title">omo Toni - 'Phases' ft. SAI's fate.</h1>
+          <p class="subheading">available everywhere now</p>
+        </section>
+        <section class="section-button">
+          <a
+            href="https://www.youtube.com/channel/UCOiVt80wJ2Nq7rcWIN4LJGg"
+            target="_blank"
+            class="button"
+          >
+            Listen on spotify
+          </a>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
