8fb6b82ad67b0b08570e8956983840c14e33bcba
Maiken Gunnerød <maiken.gunneroed@gmail.com>
2023-03-16 19:08:39 +0100

    add new basic section and website description

--- src/pages/Videopage.js
+++ src/pages/Videopage.js
@@ -20,7 +20,7 @@ function Home() {
         <div class="main-column">
           <section class="section-logo">
             <iframe
-              src="https://www.youtube.com/embed/ik4pDfveVn0"
+              src="https://www.youtube.com/embed/MSuIMOs5HXo"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -28,32 +28,32 @@ function Home() {
             ></iframe>
           </section>
           <section class="section-title">
-            <p class="song-title title">Phases</p>
-            <p class="artists title">omo Toni (ft. SAI's fate.)</p>
+            <h1 class="song-title title">On my feet</h1>
+            <p class="artists title">omo Toni</p>
           </section>
-          <section class="section-buttons">
-            <p class="subheading">listen on</p>
-            <div class="buttons">
+          <section class="section-music">
+            <h2 class="subheading">listen on</h2>
+            <div class="listen-on-buttons">
               <a
-                href="https://open.spotify.com/track/4fjP2ztoeLmp6b0ND9VTCl?si=28c70d34b85d4774"
+                href="https://open.spotify.com/track/2eBTlnsiozWMAHOnkQw0q2?si=8ac5dbebfdc24a36"
                 target="_blank"
-                class="button"
+                class="button listen-button"
                 aria-label="Spotify"
               >
                 <i class="fa-brands fa-spotify"></i>
               </a>
               <a
-                href="https://soundcloud.com/welcome-omocloud/phases"
+                href="https://soundcloud.com/welcome-omocloud/toni-sai-on-my-feet-prod-remn"
                 target="_blank"
-                class="button"
+                class="button listen-button"
                 aria-label="Soundcloud"
               >
                 <i class="fa-brands fa-soundcloud"></i>
               </a>
               <a
-                href="https://www.youtube.com/watch?v=ik4pDfveVn0&ab_channel=OMOcloud."
+                href="https://www.youtube.com/watch?v=MSuIMOs5HXo&ab_channel=OMOcloud."
                 target="_blank"
-                class="button"
+                class="button listen-button"
                 aria-label="YouTube"
               >
                 <i class="fa-brands fa-youtube"></i>
@@ -61,10 +61,46 @@ function Home() {
               <a
                 href="https://www.instagram.com/omocloud/"
                 target="_blank"
-                class="button"
+                class="button listen-button"
                 aria-label="Instagram"
               >
                 <i class="fa-brands fa-instagram"></i>
+              </a>
+            </div>
+          </section>
+          <section class="section-extra-content">
+            <h2 class="subheading">extra content</h2>
+            <div class="extra-content-embed">
+              <iframe
+                src="https://www.youtube.com/embed/cbcR7_Arp0A"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div class="buttons extra-content-buttons">
+              <p class="third-heading">still extra content</p>
+              <a
+                href="https://www.tiktok.com/@saisfate"
+                target="_blank"
+                class="button extra-content-button"
+              >
+                SAI Twitch
+              </a>
+              <a
+                href="https://www.tiktok.com/@saisfate"
+                target="_blank"
+                class="button extra-content-button"
+              >
+                SAI tiktok
+              </a>
+              <a
+                href="https://www.tiktok.com/@omotoni101"
+                target="_blank"
+                class="button extra-content-button"
+              >
+                TONI tiktok
               </a>
             </div>
           </section>
