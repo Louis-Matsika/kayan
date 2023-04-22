930e329ffd16515fa1cc48ad11b376108d0513ea
Maiken Gunnerod <maiken.gunneroed@gmail.com>
2023-04-22 22:29:53 +0200

    remove light / dark mode button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import "./App.css";
 import "./App-queries.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Videopage from "./pages/Videopage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Videopage />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
