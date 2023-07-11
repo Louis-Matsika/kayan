b9c28b6c85b4d252858d45887398b277f88638c3
Maiken Gunnerod <maiken.gunneroed@gmail.com>
2023-07-11 15:01:36 +0200

    make new waiting page and make it default

---
+++ src/pages/Waiting.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+	return (
+		<div class="waiting-div">
+			<main>
+				<img
+					src="site-content/omologo.png"
+					alt="Black OMO logo"
+					class="waiting-logo"
+				></img>
+			</main>
+		</div>
+	);
+}
+
+export default Home;
