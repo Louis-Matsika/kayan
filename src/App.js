93371b1c0dee6af3403e797be4fd74f6900cb96d
Louis Matsika <louis.matsika@bbc.co.uk>
2024-07-27 00:30:21 +0100

    multi page app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
 import "./App-queries.css";
 import "./App-waiting.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import NoMusic from "./pages/NoMusic";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Main from "./pages/Main";
+import Info from "./pages/Info";
+import NoPage from "./pages/NoPage";
 
 function App() {
 	return (
-		<Router>
+		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<NoMusic />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Main />} />
+					<Route path="Info" element={<Info />} />
+					<Route path="*" element={<NoPage />} />
+				</Route>
 			</Routes>
-		</Router>
+		</BrowserRouter>
 	);
 }
 
 export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
