70a099513653223a19c238d0c7a30d8c19b9987a
Louis Matsika <louis.matsika@bbc.co.uk>
2024-09-01 20:43:40 +0100

    remove lobby from members page - calling card

--- src/app/[cloudMember]/page.jsx
+++ src/app/[cloudMember]/page.jsx
@@ -2,14 +2,13 @@ import Lobby from "@/components/Members/Lobby/Lobby.jsx";
 import CallingCard from "@/components/Members/CallingCard/CallingCard"
 
 async function MemberPage({params}) {
-	const member = params.memberId;
+	const member = params.cloudMember;
 	return (
 		<div>
-		{params.memberId}
+		{params.cloudMember}
 		<CallingCard
-			requestedId={params.memberId} 
+			requestedId={params.cloudMember} 
 			/>
-		<Lobby />
 		</div>
 	);
 }
