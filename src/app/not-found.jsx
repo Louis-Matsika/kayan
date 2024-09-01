645208b7b295fb6a3cf3b3e5151f051b7f979497
Louis Matsika <louis.matsika@bbc.co.uk>
2024-09-01 22:54:32 +0100

    redirect 404s to home

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,5 @@
-import Link from 'next/link'
+
  
 export default function NotFound() {
-  return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  )
+  redirect(`/`);
 }
