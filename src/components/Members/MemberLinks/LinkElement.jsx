b1d46c5d5e03ef13a62a98e03f3a33b03fdca94c
Maiken Gunnerod <maiken.gunneroed@gmail.com>
2024-09-04 21:05:42 +0200

    import icons

--- src/components/Members/MemberLinks/LinkElement.jsx
+++ src/components/Members/MemberLinks/LinkElement.jsx
@@ -1,50 +1,50 @@
 // returns a social link element to be used by <Link/>
-function LinkElement({ memberLink, platformId }) {
-	let platform = memberLink;
-	let platformIcon;
+function LinkElement({memberLink, platformId}) {
+    let platform = memberLink;
+    let platformIcon;
 
-	switch (platformId) {
-		case 0:
-			platform = "YouTube";
-			platformIcon = "youtube";
-			break;
-		case 1:
-			platform = "SoundCloud";
-			platformIcon = "soundcloud";
-			break;
-		case 2:
-			platform = "Spotify";
-			platformIcon = "spotify";
-			break;
-		case 3:
-			platform = "Tiktok";
-			platformIcon = "tiktok";
-			break;
-		case 4:
-			platform = "Instagram";
-			platformIcon = "instagram";
-			break;
-		case 5:
-			platform = "Snapchat";
-			platformIcon = "snapchat";
-			break;
-		default:
-			break;
-	}
+    switch (platformId) {
+        case 0:
+            platform = "YouTube";
+            platformIcon = "youtube";
+            break;
+        case 1:
+            platform = "SoundCloud";
+            platformIcon = "soundcloud";
+            break;
+        case 2:
+            platform = "Spotify";
+            platformIcon = "spotify";
+            break;
+        case 3:
+            platform = "Tiktok";
+            platformIcon = "tiktok";
+            break;
+        case 4:
+            platform = "Instagram";
+            platformIcon = "instagram";
+            break;
+        case 5:
+            platform = "Snapchat";
+            platformIcon = "snapchat";
+            break;
+        default:
+            break;
+    }
 
-	return (
-		<>	
-			{memberLink !== "" && (
-				//if provided link is not empty, then make a list element
-				<li>
-					<a className="link" href={memberLink} target="_blank" rel="noopener">
-						<i className={"fa-brands fa-snapchat"}>fix icons</i>
-						<p>{platform}</p>
-					</a>
-				</li>
-			)}
-		</>
-	);
+    return (
+        <>
+            {memberLink !== "" && (
+                //if provided link is not empty, then make a list element
+                <li>
+                    <a className="link" href={memberLink} target="_blank" rel="noopener">
+                        <i className={"fa-brands fa-snapchat"}>fix icons</i>
+                        <p>{platform}</p>
+                    </a>
+                </li>
+            )}
+        </>
+    );
 }
 
 export default LinkElement;
