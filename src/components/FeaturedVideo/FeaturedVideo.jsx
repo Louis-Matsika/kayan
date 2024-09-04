d30cc4dd5c899c28b15748cb3ad4e00d3df4cbef
Louis Matsika <louis.matsika@bbc.co.uk>
2024-09-04 14:59:48 +0100

    add featured video logic

--- src/components/FeaturedVideo/FeaturedVideo.jsx
+++ src/components/FeaturedVideo/FeaturedVideo.jsx
@@ -13,9 +13,36 @@ async function FeaturedVideo({requestedId,}) {
     return(
         <>
         {videos.map(FeaturedVideo => (
-            <div  key={requestedId} className="featured-video-box content-box">
-				{JSON.stringify(FeaturedVideo)}
+            <div key ={requestedId} className="featured-video-box content-box">
+			<iframe
+				className="featured-video-embed"
+				src={FeaturedVideo.embed}
+				title="YouTube video player"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerPolicy="strict-origin-when-cross-origin"
+				allowFullScreen
+			></iframe>
+			<div className="featured-right content-right">
+				<p className="featured-title content-title">{FeaturedVideo.title}</p>
+				<p className="featured-artist">{FeaturedVideo.name}</p>
+				<p className="featured-description">{FeaturedVideo.description}</p>
+				<div>
+					<ul className="link-list">
+						<li>
+							<a
+								className="link"
+								href={FeaturedVideo.link}
+								target="_blank"
+								rel="noopener"
+							>
+								<i className="fa-brands fa-youtube"></i>
+								<p>YouTube</p>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
+		</div>
         ))}
         </>
     );
