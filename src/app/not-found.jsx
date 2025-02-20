cbe9cea3f76097dc99e475a56af919181ab6b9b9
Louis Matsika <louis.matsika@bbc.co.uk>
2025-02-20 20:18:51 +0000

    use fate tech stack, app restart

--- src/app/not-found.jsx
+++
@@ -1,6 +0,0 @@
-//404 page
-import { redirect } from 'next/navigation'
- 
-export default function NotFound() {
-  redirect(`/`);
-}
--- src/components/fetchVideoData.jsx
+++
@@ -1,9 +0,0 @@
-//the api call that fetches OMOcloud member video data currently on jsonbin
-
-async function fetchData(){
-    const res = await fetch("https://api.jsonbin.io/v3/b/66d85ea4e41b4d34e42a298b?meta=false", {next: {revalidate: 1}});
-
-    return(res.json());
-}
-
-export default fetchData;
