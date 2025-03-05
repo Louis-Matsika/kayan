b43aa698ff6b01e01a475ca8c09e2d6613e69ba6
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-05 20:36:18 +0000

    add emblem logic

--- src/components/Lobby/index.tsx
+++ src/components/Lobby/index.tsx
@@ -11,7 +11,7 @@ const Lobby = () => {
   return (
     <div className={styles.lobby}>
       {members.map((member) => (
-        <LobbyRow key={member.id} name={member.name} id={member.id}bio={member.bio} />
+        <LobbyRow key={member.id} name={member.name} username={member.username} id={member.id}bio={member.bio} />
       ))}
     </div>
   );
--- src/components/LobbyRow/index.tsx
+++ src/components/LobbyRow/index.tsx
@@ -1,19 +1,24 @@
 import styles from "./index.module.scss";
 interface LobbyRowProps {
   name: String;
+  username: String;
   id: String;
   bio: String;
 }
 
-const LobbyRow = ({ name, id, bio }: LobbyRowProps) => {
+const LobbyRow = ({ name, username, id, bio }: LobbyRowProps) => {
   return (
     <div className={styles.lobbyRow}>
       <details>
         <summary>
-
-            <span>{name}</span>
-            <span><img src={'/test-img.jpg'}/><p>{id}</p></span>
-
+          <span>{name}</span>
+          <span>
+            <img
+              src={`/member-assets/emblems/${username}-emblem.png`}
+              alt={`${name}'s emblem`}
+            ></img>
+            <p>{id}</p>
+          </span>
         </summary>
         <div>
           <p>{bio}</p>
