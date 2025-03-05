c635e04943cbc33fa2eb67b2404a93947bfabb06
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-05 18:13:05 +0000

    add loby .scss file

--- src/components/Lobby/index.tsx
+++ src/components/Lobby/index.tsx
@@ -3,11 +3,13 @@ import LobbyRow from "#/components/LobbyRow";
 
 import OMOcloud from "#/data/OMOcloud.json";
 
+import styles from './index.module.scss';
+
 const members = OMOcloud.members;
 
 const Lobby = () => {
   return (
-    <div>
+    <div className={styles.lobby}>
       {members.map((member) => (
         <LobbyRow key={member.id} name={member.name} bio={member.bio} />
       ))}
