596c6902e3731d99c66d86925fc40eeb429ecbad
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-05 17:48:19 +0000

    start getting sassy!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+
 import Header from "#/components/Header/Header";
 import Lobby from '#/components/Lobby';
 import Footer from "#/components/Footer/Footer";
+
+import "#theme/global.scss";
 
 const Home=()=> {
   return (
