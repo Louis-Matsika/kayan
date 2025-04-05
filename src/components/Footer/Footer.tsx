c3b823768a7d0261d59bc0b7947678442812b6f9
Louis Matsika <louis.matsika@bbc.co.uk>
2025-04-05 21:16:52 +0100

    fit footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <a className={styles.link} href="https://github.com/Louis-Matsika/fate" target="_blank" rel="noopener noreferrer">
+            <a className={styles.link} href="https://github.com/Louis-Matsika/kayan" target="_blank" rel="noopener noreferrer">
                 source code
             </a>
         </footer>
